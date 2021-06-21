49178245a8667fcab2f363ac8deeb3bd25d1dd87
PIYUSH MALI <70318450+piyushmali@users.noreply.github.com>
2021-06-21 22:15:38 +0530

    Add files via upload

---
+++ cal.py
@@ -0,0 +1,4 @@
+import calendar
+theyear = 2021
+themonth = 6
+print(calendar.month(theyear, themonth))
---
+++ piechart.py
@@ -0,0 +1,6 @@
+# pip install matplotlib
+import matplotlib.pyplot as p
+s = [40, 30, 20, 10]
+l = ["Samosa", "Kachori", "Aloobada", "Moongbada"]
+p.pie(s, labels=l)
+p.show()
